3357bfaf2c3852132de3f9160bb6925e4b3edfc6
teafoot <davidchenlog@gmail.com>
2021-04-27 17:19:31 -0700

    Redux - logout
    Home - ImgSlider component (slideshow)
    Home - Viewers component (video plays in background)

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import {auth, provider} from "../firebase";
 import {useHistory} from "react-router-dom"
 import {useDispatch, useSelector} from "react-redux";
-import {selectUserName, selectUserPhoto, setUserLoginDetails} from "../features/user/userSlice";
+import {selectUserName, selectUserPhoto, setUserLoginDetails, setSignOutState} from "../features/user/userSlice";
 import { useEffect } from "react";
 
 const Header = (props) => {
@@ -21,20 +21,30 @@ const Header = (props) => {
     }, [userName]); // redirect to /home when state.user.name is set (logged in), instead of / which is the Login component
 
     const handleAuth = () => {
-        auth
-            .signInWithPopup(provider)
-            .then((result) => {
-                // console.log(result);
-                setUser(result.user);
-            })
-            .catch((error) => {
-                alert(error.message);
-            });
+        if (!userName) {
+            auth
+                .signInWithPopup(provider)
+                .then((result) => {
+                    // console.log(result);
+                    setUser(result.user);
+                })
+                .catch((error) => {
+                    alert(error.message);
+                });
+        } else if (userName) {
+            auth
+                .signOut()
+                .then(() => {
+                    dispatch(setSignOutState()); // action
+                    history.push("/"); // login page
+                })
+                .catch((err) => alert(err.message));
+        }
     };
 
     const setUser = (user) => {
         dispatch(
-            setUserLoginDetails({
+            setUserLoginDetails({ // action
                 name: user.displayName,
                 email: user.email,
                 photo: user.photoURL,
@@ -75,7 +85,12 @@ const Header = (props) => {
                             <span>SERIES</span>
                         </a>
                     </NavMenu>
-                    <UserImg src={userPhoto} alt={userName}/>
+                    <SignOut>
+                        <UserImg src={userPhoto} alt={userName} />
+                        <DropDown>
+                            <span onClick={handleAuth}>Sign out</span>
+                        </DropDown>
+                    </SignOut>
                 </>
             }
         </Nav>
@@ -194,6 +209,44 @@ const Login = styled.a` // button for login
 
 const UserImg = styled.img` // user image after login
   height: 100%;
+`;
+
+const DropDown = styled.div`
+  position: absolute;
+  top: 48px;
+  right: 0px;
+  background: rgb(19, 19, 19);
+  border: 1px solid rgba(151, 151, 151, 0.34);
+  border-radius: 4px;
+  box-shadow: rgb(0 0 0 / 50%) 0px 0px 18px 0px;
+  padding: 10px;
+  font-size: 14px;
+  letter-spacing: 3px;
+  width: 100px;
+  opacity: 0;
+`;
+
+const SignOut = styled.div`
+  position: relative;
+  height: 48px;
+  width: 48px;
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+
+  ${UserImg} {
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+  }
+
+  &:hover {
+    ${DropDown} {
+      opacity: 1;
+      transition-duration: 1s;
+    }
+  }
 `;
 
 export default Header;
